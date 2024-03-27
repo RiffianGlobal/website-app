d946887b5f455061559ac24b24a901c010ca618b
Dalle <Dalle>
2024-03-27 15:12:20 +0800

    release v1.0.2

---
+++ assets/action-j50kmG4w.js
@@ -0,0 +1,19 @@
+import{l as r}from"./index-wqnKB3n-.js";const t=(e,a={})=>r("MediaBoard",u(e,a)),o=e=>r("MediaBoard",d(e)),d=e=>`{
+  user ( id: "${e.toLowerCase()}" ) {
+    address, holding, rewardClaimed
+    socials {
+      id, pid, uri, platform
+    }
+    subjectsCreated {
+      id
+    }
+  }
+}`,u=(e,{first:a=0,skip:s=0}={})=>`{
+  user ( id: "${e.toLowerCase()}" ) {
+    subjectsCreated(orderBy: supply, orderDirection: desc${a>0?`, first: ${a}`:""}${s>0?`, ${s}`:""}) {
+      address, createdAt, name, image, uri
+      fansNumber, supply, totalVoteValue
+      volumeTotal, volumeRetreat, volumeVote, lastVoteAt
+    }
+  }
+}`,c=async e=>await t(e),l=async e=>await o(e);export{c as t,l as u};
